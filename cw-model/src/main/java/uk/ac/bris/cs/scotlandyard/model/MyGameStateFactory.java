5f980af6c3758cba3616beaacfb1ca7e61e8a238
casperking666 <>
2021-04-14 00:49:12 +0100

    PlayerTest done, 62 tests passed could merge

--- cw-model/src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
+++ cw-model/src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -191,7 +191,7 @@ public final class MyGameStateFactory implements Factory<GameState> {
 			Function<DoubleMove, Integer> dmf1 = x -> x.destination1;
 			FunctionalVisitor<Integer> getDestination = new FunctionalVisitor<>(smf1, dmf1);
 
-			Function<SingleMove, Integer> smf2 = x -> x.destination;
+			Function<SingleMove, Integer> smf2 = x -> x.destination; // think this is repetitive
 			Function<DoubleMove, Integer> dmf2 = x -> x.destination2;
 			FunctionalVisitor<Integer> getDestination2 = new FunctionalVisitor<>(smf2, dmf2);
 
@@ -200,7 +200,7 @@ public final class MyGameStateFactory implements Factory<GameState> {
 			FunctionalVisitor<Ticket> getTicket1 = new FunctionalVisitor<>(smt1, dmt1);
 
 
-			Function<SingleMove, Ticket> smt2 = x -> x.ticket;
+			Function<SingleMove, Ticket> smt2 = x -> x.ticket; // same as this
 			Function<DoubleMove, Ticket> dmt2 = x -> x.ticket2;
 			FunctionalVisitor<Ticket> getTicket2 = new FunctionalVisitor<>(smt2, dmt2);
 			//Integer destination = move.visit(getDestination);
@@ -217,7 +217,9 @@ public final class MyGameStateFactory implements Factory<GameState> {
 			if(move.commencedBy().isDetective()){
 				for(int i = 0; i < newDetectives.size(); i++){
 					if(newDetectives.get(i).piece().equals(move.commencedBy())){
-						newDetectives.set(i, newDetectives.get(i).use(move.visit(getTicket1)));
+						// newDetectives.set(i, newDetectives.get(i).use(move.visit(getTicket1)));
+						Player tempDetective = newDetectives.get(i);
+						newDetectives.set(i, tempDetective.use(move.visit(getTicket1)));
 						detectives = ImmutableList.copyOf(newDetectives);
 						mrX = mrX.give(move.visit(getTicket1));
 					}
@@ -253,6 +255,7 @@ public final class MyGameStateFactory implements Factory<GameState> {
 
 
 			// Travel Log
+			/*
 			if(move.commencedBy().isMrX()){
 				var aLog = new ArrayList<LogEntry>();
 				aLog.addAll(this.log);
@@ -278,32 +281,33 @@ public final class MyGameStateFactory implements Factory<GameState> {
 					}nt++;
 				}
 				this.log = ImmutableList.copyOf(aLog);
+			}*/
+
+			if (move.commencedBy().isMrX()) {
+				int size = log.size();
+				var tempLog = new ArrayList<LogEntry>();
+				tempLog.addAll(this.log);
+				if (move.visit(getSingleOrDouble).equals("SingleMove")) {
+					if (setup.rounds.get(size)) {
+						tempLog.add(LogEntry.reveal(move.visit(getTicket1), destination));
+					}
+					else tempLog.add(LogEntry.hidden(move.visit(getTicket1)));
+				}
+				else {
+					if (setup.rounds.get(size)) {
+						tempLog.add(LogEntry.reveal(move.visit(getTicket1), destination1));
+					}
+					else {tempLog.add(LogEntry.hidden(move.visit(getTicket1)));}
+					// size is 1 bigger because the log size won't be updated here, but since we just add
+					// another logEntry above, the size hence should add 1 to it.
+					if (setup.rounds.get(size + 1)) {
+						tempLog.add(LogEntry.reveal(move.visit(getTicket2), destination2));
+					}
+					else {tempLog.add(LogEntry.hidden(move.visit(getTicket2)));}
+				}
+				this.log = ImmutableList.copyOf(tempLog);
 			}
 			//Travel Log ends
-
-
-
-			//Player locations
-			//if(move.commencedBy().isMrX()){
-
-			//	mrX = mrX.at(destination);
-			//}
-			/*
-			else if(move.commencedBy().isDetective()){
-				for(int i = 0; i < detectives.size(); i++){
-					if(detectives.get(i).piece().equals(move.commencedBy())){
-						detectives.add(i, detectives.get(i).at(destination));
-						detectives.remove(i+1);
-					}
-				}
-			}*/
-			// 你上头写得这个有点小bug 我改了一下
-			/*else if (move.commencedBy().isDetective()) {
-				for (Player detective : detectives) {
-					if (detective.piece().equals(move.commencedBy()))
-						detective.at(destination);
-				}
-			}*/
 
 			// might still have bugs, please don't change anything
 			var container = new ArrayList<Piece>();
